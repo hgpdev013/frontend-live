1035efc5731a05a201fb17e8106c3e8e34449ebc
Hugo <hugo.henrique@gbmlogistica.com.br>
2024-02-09 14:55:33 -0300

    Initial Commit

---
+++ src/pages/List/index.tsx
@@ -0,0 +1,8 @@
+import * as Styles from "./styles";
+
+export default function ListPage() {
+
+  return <Styles.Container>
+    Hello World!
+  </Styles.Container>;
+}
---
+++ src/styles/index.ts
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyleLayout = createGlobalStyle`
+  color: #000;
+  background-color: #fff;
+  font-family: 'Inter', sans-serif;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+`;
